a920798ffe6a2fa2e3e260fed7b43bdb9c6c11d2
matthew0506 <matthew0506@godaddy.com>
2023-03-29 10:57:30 -0700

    project 3 intial dump

---
+++ Gutierrez-Project3/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import {
+          StyleSheet,
+          Text,
+          TextInput,
+          TouchableOpacity,
+          View
+        } from 'react-native';
+
+export default class App extends Component {
+              state = {
+                results: '',
+              };
+
+              onLoad = async () => {
+                this.setState({ results: 'Loading, please wait...' });
+                const response = await
+                fetch('http://jsonplaceholder.typicode.com/users', {
+                  method: 'GET',
+                });
+                const results = await response.text();
+                this.setState({ results });
+              }
+
+              render() {
+                const { results } = this.state;
+                return (
+                  <View style={styles.container}>
+                    <View>
+                      <TextInput
+                        style={styles.preview}
+                        value={results}
+                        placeholder="Results..."
+                        editable={false}
+                        multiline
+                      />
+                      <TouchableOpacity onPress={this.onLoad} style=
+                      {styles.btn}>
+                        <Text>Load data</Text>
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                );
+              }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  preview: {
+    backgroundColor: '#bdc3c7',
+    width: 300,
+    height: 400,
+    padding: 10,
+    borderRadius: 5,
+    color: '#333',
+    marginBottom: 50,
+  },
+  btn: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    borderRadius: 3,
+    marginTop: 10,
+  },
+});
