c3d0030989c19feadcb3187c76cac2d16b3d2fad
matthew0506 <matthew0506@godaddy.com>
2023-03-30 13:56:02 -0700

    assignement 3 done

--- Gutierrez-Project3/App.js
+++ Gutierrez-Project3/App.js
@@ -8,40 +8,72 @@ import {
         } from 'react-native';
 
 export default class App extends Component {
-              state = {
-                results: '',
-              };
+  state = {
+    results: '',
+  };
 
-              onLoad = async () => {
-                this.setState({ results: 'Loading, please wait...' });
-                const response = await
-                fetch('http://jsonplaceholder.typicode.com/users', {
-                  method: 'GET',
-                });
-                const results = await response.text();
-                this.setState({ results });
-              }
+  onLoad = async (query) => {
+    this.setState({ results: 'Loading, please wait...' });
+    fetch('https://2s4b8wlhik.execute-api.us-east-1.amazonaws.com/studentData?grade=' + query, {
+        method: 'GET',
+      })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else if (response.status == 400) {
+          throw new Error("Invalid query");
+        } else {
+          throw new Error("Error fetching data");
+        }
+      })
+      .then(results => {
+        const a = results.join('\n')
+        this.setState({ results: a })
+      }).catch(error =>  {
+        this.setState({ results: error.message});
+      })
+  }
 
-              render() {
-                const { results } = this.state;
-                return (
-                  <View style={styles.container}>
-                    <View>
-                      <TextInput
-                        style={styles.preview}
-                        value={results}
-                        placeholder="Results..."
-                        editable={false}
-                        multiline
-                      />
-                      <TouchableOpacity onPress={this.onLoad} style=
-                      {styles.btn}>
-                        <Text>Load data</Text>
-                      </TouchableOpacity>
-                    </View>
-                  </View>
-                );
-              }
+  render() {
+    const { results } = this.state;
+    return (
+      <View style={styles.container}>
+        <View>
+          <TextInput
+            style={styles.preview}
+            value={results}
+            placeholder="Results..."
+            editable={false}
+            multiline
+          />
+          <TouchableOpacity onPress={() => this.onLoad('')} style=
+          {styles.btn}>
+            <Text>Load data</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.onLoad('A')} style=
+          {styles.btn}>
+            <Text>A</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.onLoad('B')} style=
+          {styles.btn}>
+            <Text>B</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.onLoad('C')} style=
+          {styles.btn}>
+            <Text>C</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.onLoad('D')} style=
+          {styles.btn}>
+            <Text>D</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.onLoad('E')} style=
+          {styles.btn}>
+            <Text>E</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
