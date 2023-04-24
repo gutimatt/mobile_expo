235d74c26a4847c0ae73657069be68e3ccb37758
matthew0506 <matthew0506@godaddy.com>
2023-04-24 11:35:22 -0700

    add textarea

--- Gutierrez-Project6/components/Album/index.js
+++ Gutierrez-Project6/components/Album/index.js
@@ -1,5 +1,40 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View
+  } from 'react-native';
 
-export default class Album extends Component {
-    render() { }
+
+const Album = () => {
+    const [text, onChangeText] = useState();
+
+    return (
+        <SafeAreaView>
+            <TextInput
+                style={styles.preview}
+                value={text}
+                onChangeText={onChangeText}
+                // placeholder="Results..."
+                editable={true}
+            /> 
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+  preview: {
+    backgroundColor: '#bdc3c7',
+    width: 300,
+    // height: 400,
+    padding: 10,
+    borderRadius: 5,
+    color: '#333',
+    marginBottom: 50,
+  }
+})
+
+export default Album;
